d4dd3e8f8b0c07a847d600a3c02fd85f3307e967
Andy Babic <ababic@rkh.co.uk>
2015-06-17 17:32:07 +0100

    Format as package with setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from wagtailmodeladmin import __version__
+
+setup(
+    name="wagtailmodeladmin",
+    version=__version__,
+    author="Andy Babic",
+    author_email="ababic@rkh.co.uk",
+    description="Customisable 'django-admin' style listing pages for Wagtail CMS",
+    license="MIT",
+    keywords="wagtail cms model utility",
+    url="https://github.com/ababic/wagtailmodeladmin",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Topic :: Internet :: WWW/HTTP',
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    install_requires=[
+        'wagtail>=1.0',
+    ]
+)
