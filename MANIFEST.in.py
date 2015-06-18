b3f8df33086380e9b426424cf504194a58207d85
Andy Babic <ababic@rkh.co.uk>
2015-06-18 10:58:04 +0100

    follow django packaging guidelines

---
+++ MANIFEST.in.py
@@ -0,0 +1,5 @@
+include LICENSE
+include README.rst
+recursive-include docs *
+recursive-include wagtailmodeladmin/static *
+recursive-include wagtailmodeladmin/templates *
--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from setuptools import find_packages, setup
+import os
+from setuptools import setup, find_packages
 from wagtailmodeladmin import __version__
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="wagtailmodeladmin",
     version=__version__,
     author="Andy Babic",
     author_email="ababic@rkh.co.uk",
-    description="Customisable 'django-admin' style listing pages for Wagtail CMS",
+    description="Customisable 'django-admin' style listing pages for Wagtail",
+    long_description=README,
+    packages=find_packages(),
     license="MIT",
     keywords="wagtail cms model utility",
     url="https://github.com/ababic/wagtailmodeladmin",
-    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
@@ -22,5 +30,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    install_requires=[]
+    #install_requires=['wagtail>=0.8.7'],
 )
