697a0c39b30e0d2c6caa54de7c2a925e75b000a9
Andy Babic <ababic@rkh.co.uk>
2015-10-07 15:17:23 +0100

    PyPi additions

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     packages=find_packages(),
     license="MIT",
     keywords="wagtail cms model utility",
+    download_url="https://github.com/ababic/wagtailmodeladmin/tarball/0.1",
     url="https://github.com/ababic/wagtailmodeladmin",
     include_package_data=True,
     zip_safe=False,
