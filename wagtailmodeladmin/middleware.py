728b495c6bb7e60c0fc4636ad26b743c0685e06d
Andy Babic <ababic@rkh.co.uk>
2015-11-10 10:37:08 +0000

    Fixes #22

--- wagtailmodeladmin/middleware.py
+++ wagtailmodeladmin/middleware.py
@@ -12,37 +12,56 @@ class ModelAdminMiddleware(object):
     """
 
     def process_request(self, request):
-        referer_url = request.META.get('HTTP_REFERER')
-        return_to_index_url = request.session.get('return_to_index_url')
+        """
+        Ignore unnecessary actions for static file requests, posts, or ajax
+        requests. We're only interested in redirecting following a 'natural'
+        request redirection within the admin area.
+        """
+        if all([
+            request.path.startswith('/admin/'),
+            request.method == 'GET',
+            not request.is_ajax()
+        ]):
+            referer_url = request.META.get('HTTP_REFERER')
+            return_to_index_url = request.session.get('return_to_index_url')
 
-        try:
-            resolver_match = resolve(request.path)
+            try:
+                """
+                We only want to redirect if we're on the 'wagtailadmin_explore'
+                url, and we have something to redirect to.
+                """
+                resolver_match = resolve(request.path)
+                if all((
+                    resolver_match.url_name == 'wagtailadmin_explore',
+                    return_to_index_url,
+                    referer_url)
+                ):
+                    referer_match = resolve(urlparse(referer_url).path)
+                    referer_name = referer_match.url_name
+                    referer_ns = referer_match.namespace
 
-            if all((
-                resolver_match.url_name == 'wagtailadmin_explore',
-                return_to_index_url,
-                referer_url)
-            ):
+                    perform_redirection = False
+                    if all([
+                        referer_ns == 'wagtailadmin_pages',
+                        referer_name in ('add', 'edit', 'delete', 'unpublish',
+                                         'copy'),
+                    ]):
+                        perform_redirection = True
+                    elif all([
+                        not referer_ns,
+                        referer_name in (
+                            'wagtailadmin_pages_create',
+                            'wagtailadmin_pages_edit',
+                            'wagtailadmin_pages_delete',
+                            'wagtailadmin_pages_unpublish'
+                        ),
+                    ]):
+                        perform_redirection = True
+                    if perform_redirection:
+                        del request.session['return_to_index_url']
+                        return HttpResponseRedirect(return_to_index_url)
 
-                referer_match = resolve(urlparse(referer_url).path)
-                referer_name = referer_match.url_name
-                referer_ns = referer_match.namespace
-
-                if any((
-                    referer_ns == 'wagtailadmin_pages' and referer_name in (
-                        'add', 'edit', 'delete', 'unpublish', 'copy'
-                    ),
-                    not referer_ns and referer_name in (
-                        'wagtailadmin_pages_create',
-                        'wagtailadmin_pages_edit',
-                        'wagtailadmin_pages_delete',
-                        'wagtailadmin_pages_unpublish',
-                    ),
-                )):
-                    del request.session['return_to_index_url']
-                    return HttpResponseRedirect(return_to_index_url)
-
-        except Resolver404:
-            pass
+            except Resolver404:
+                pass
 
         return None
