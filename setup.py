907596cb6dfa9adc87568c2dd89684d2a0725ab2
Andy Babic <ababic@rkh.co.uk>
2015-12-18 14:47:19 +0000

    Delete duplicate README and adjust MANIFEST.in again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 from wagtailmodeladmin import __version__
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
