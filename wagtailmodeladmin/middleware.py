4d56a2b4c372d12bc52ab4907a329e15537b197c
Andy Babic <ababic@rkh.co.uk>
2015-12-21 11:22:46 +0000

    Simplified ModelAdminMiddleware. Fixes #29

--- wagtailmodeladmin/middleware.py
+++ wagtailmodeladmin/middleware.py
@@ -1,12 +1,12 @@
 from django.utils.six.moves.urllib.parse import urlparse
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import resolve, Resolver404
+from django.core.urlresolvers import resolve, reverse, Resolver404
 
 
 class ModelAdminMiddleware(object):
     """
     Whenever loading wagtail's wagtailadmin_explore url, we check the session
-    for a 'return_to_list_url' value (set by some views), to see if the user
+    for a `return_to_list_url` value (set by some views), to see if the user
     should be redirected to a custom list view instead, and if so, redirect
     them to it.
     """
@@ -15,47 +15,46 @@ class ModelAdminMiddleware(object):
         """
         Ignore unnecessary actions for static file requests, posts, or ajax
         requests. We're only interested in redirecting following a 'natural'
-        request redirection within the admin area.
+        request redirection to the `wagtailadmin_explore_root` or
+        `wagtailadmin_explore` views.
         """
-        if all([
-            request.path.startswith('/admin/'),
-            request.method == 'GET',
-            not request.is_ajax()
-        ]):
-            referer_url = request.META.get('HTTP_REFERER')
-            return_to_index_url = request.session.get('return_to_index_url')
+        referer_url = request.META.get('HTTP_REFERER')
+        return_to_index_url = request.session.get('return_to_index_url')
 
+        if all([
+            return_to_index_url,
+            referer_url,
+            request.method == 'GET',
+            not request.is_ajax(),
+            request.path.startswith(reverse('wagtailadmin_explore_root')),
+        ]):
             try:
-                """
-                We only want to redirect if we're on the 'wagtailadmin_explore'
-                view, and we have something to redirect to.
-                """
+                perform_redirection = False
+                referer_match = resolve(urlparse(referer_url).path)
                 if all((
-                    return_to_index_url,
-                    referer_url,
-                    resolve(request.path).url_name == 'wagtailadmin_explore',
+                    referer_match.namespace == 'wagtailadmin_pages',
+                    referer_match.url_name in (
+                        'add',
+                        'edit',
+                        'delete',
+                        'unpublish',
+                        'copy'
+                    ),
                 )):
-                    perform_redirection = False
-                    referer_match = resolve(urlparse(referer_url).path)
-                    if all((
-                        referer_match.namespace == 'wagtailadmin_pages',
-                        referer_match.url_name in (
-                            'add', 'edit', 'delete', 'unpublish', 'copy'),
-                    )):
-                        perform_redirection = True
-                    elif all((
-                        not referer_match.namespace,
-                        referer_match.url_name in (
-                            'wagtailadmin_pages_create',
-                            'wagtailadmin_pages_edit',
-                            'wagtailadmin_pages_delete',
-                            'wagtailadmin_pages_unpublish'
-                        ),
-                    )):
-                        perform_redirection = True
-                    if perform_redirection:
-                        del request.session['return_to_index_url']
-                        return HttpResponseRedirect(return_to_index_url)
+                    perform_redirection = True
+                elif all((
+                    not referer_match.namespace,
+                    referer_match.url_name in (
+                        'wagtailadmin_pages_create',
+                        'wagtailadmin_pages_edit',
+                        'wagtailadmin_pages_delete',
+                        'wagtailadmin_pages_unpublish'
+                    ),
+                )):
+                    perform_redirection = True
+                if perform_redirection:
+                    del request.session['return_to_index_url']
+                    return HttpResponseRedirect(return_to_index_url)
 
             except Resolver404:
                 pass
