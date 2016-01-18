f9a17305f7dc540d091d590fe852b6f64d553475
Andy Babic <ababic@rkh.co.uk>
2016-01-18 16:12:01 +0000

    Update urls in settings and bump version once more

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     packages=find_packages(),
     license="MIT",
     keywords="wagtail cms model utility",
-    download_url="https://github.com/ababic/wagtailmodeladmin/tarball/0.1",
-    url="https://github.com/ababic/wagtailmodeladmin",
+    download_url="https://github.com/rkhleics/wagtailmodeladmin/tarball/0.1",
+    url="https://github.com/rkhleics/wagtailmodeladmin",
     include_package_data=True,
     zip_safe=False,
     classifiers=[
